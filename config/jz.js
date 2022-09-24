edf7dce6d2f540ec8b987a3958bf4ce7af2eaa65
Diiz98 <114228778+Diiz98@users.noreply.github.com>
2022-09-24 14:39:48 +0800

    Update jz.js

--- config/jz.js
+++ config/jz.js
@@ -19,7 +19,7 @@ export const CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oIFqC6l9qevcWs84ZmXWCrQrvBpM“,
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "KkxFACB0q6Z-8SgtLiFN7NCdeF_NQM6Wo4VczdTF2Sc",
+      useTemplateId: "lgb3INCy_A-1zMmXBRPyVSc34KcSA6B1-lN7Okn7-ZE",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       province: "甘肃",
       city: "兰州",
@@ -38,7 +38,7 @@ export const CONFIG = {
   
 
   FESTIVALS: [
-    {"type": "*生日", "name": "坏男人", "year": "1997", "date": "r07-18", isShowAge: true},
+    {"type": "*生日", "name": "坏男人", "year": "1997", "date": "07-18"},
   ],
   
   /**
@@ -52,24 +52,10 @@ export const CONFIG = {
   
   CUSTOMIZED_DATE_LIST: [
     // 我喜欢上你的日子
-    {"keyword": "love_day", date: "2022-07-11", color: '#f0b4ee'}, 
+    {"keyword": "love_day", date: "2022-07-11"}, 
   ],
   
-  /** 插槽 */
-  
-  /** 你可以在这里写超多的你想显示的内容了！
-   * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
-   * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }}
-   * */
-SLOT_LIST: [
-    // 这样配置的话，就会每次随机选一句话发送
-    {"keyword": "lover_prattle", contents: [
-        "申请成为你爱里的永久居民。",
-        "真的希望有一天能等得到你",
-        "今天又是点点想你的一天哦",
-      ], color: '#6f6041'},
- 
-  ],
+
   // sakana日
     // {"keyword": "sakana_day", date: "2022-01-06"},
     // ... 
